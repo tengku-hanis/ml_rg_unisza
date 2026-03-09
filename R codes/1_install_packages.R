1f290847f4082e389244497f091dfedb7d97fd5b
tengku-hanis <tengkuhanismokhtar@gmail.com>
2026-03-09 16:24:40 +0800

    initial commit

---
+++ R codes/1_install_packages.R
@@ -0,0 +1,28 @@
+##=============================================================================##
+## Title: Install related packages
+## Author: Tengku Muhd Hanis Bin Tengku Mokhtar, PhD
+## Date: March 10, 2026
+##=============================================================================##
+
+# Install packages
+install.packages("tidyverse")
+install.packages("mlbench")
+install.packages("skimr")
+install.packages("rpart.plot")
+install.packages("vip")
+install.packages("janitor")
+install.packages("naniar")
+install.packages("DataExplorer")
+
+# Refresh session before installing the packages below
+install.packages("tidymodels")
+install.packages("discrim")
+install.packages("mda")
+install.packages("earth")
+install.packages("randomForest")
+install.packages("stacks")
+install.packages("finetune")
+install.packages("xgboost")
+
+
+
